38f61b58ed33ea17b8f95878d8a39607a43e784f
MilaGttP <milaivanova2609@gmail.com>
2023-09-30 11:55:38 +0300

    Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Timer from './Components/Timer';
 
 function App() {
   return (
     <div className="App">
-      
+      <Timer name="Europe/Kiev" />
+      <Timer name="America/New_York" />
+      <Timer name="Asia/Hong_Kong" />
     </div>
   );
 }
